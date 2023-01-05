14fef99a30a383967f7dc9bbf323f90554c73387
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-01-05 19:02:19 +0530

    Modified to Create Linked List program

--- DataStructureDay14/Program.cs
+++ DataStructureDay14/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data Structure programs");
+            Console.WriteLine("Please enter option given below");
+            Console.WriteLine("Enter/n1:Add LinkedList");
+            int option = Convert.ToInt32(Console.ReadLine());
+            LinkedList linkedlist = new LinkedList();
+            switch (option)
+            {
+                case 1:
+                    linkedlist.AddLast(56);
+                    linkedlist.AddLast(30);
+                    linkedlist.AddLast(70);
+                    linkedlist.Display();
+                    break;
+            }
         }
     }
 }
